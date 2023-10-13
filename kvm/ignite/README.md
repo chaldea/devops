285d4d83d72b6ca173da1d2189246f6316eb6c56
yoli <yoli@chaldea.cn>
2023-10-13 06:06:07 -0400

    add ignite

---
+++ kvm/ignite/README.md
@@ -0,0 +1,9 @@
+### Run VM
+```bash
+ignite run weaveworks/ignite-centos:7-v0.10.0 \
+--name demo \
+--cpus 2 \
+--memory 1GB \
+--size 10GB \
+--ssh
+```
---
+++ kvm/ignite/install.sh
@@ -0,0 +1,9 @@
+export VERSION=v0.10.0
+export GOARCH=$(go env GOARCH 2>/dev/null || echo "amd64")
+
+for binary in ignite ignited; do
+    echo "Installing ${binary}..."
+    curl -sfLo ${binary} https://github.com/weaveworks/ignite/releases/download/${VERSION}/${binary}-${GOARCH}
+    chmod +x ${binary}
+    sudo mv ${binary} /usr/local/bin
+done
